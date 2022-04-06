1a051eb4f6f6084bc8b15e3aaa0e57616793f7f9
Brandon Lin <bwlin93@gmail.com>
2022-04-06 13:47:31 -0500

    Connects to product DB, displays images, and formats products on page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,52 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
       display: false,
-      products: [
-        { id: 1, name: "Pillow", price: 25 },
-        { id: 2, name: "Chair", price: 100 },
-      ],
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <p v-for="product in products" :key="product.id">{{ product.name }}</p>
+    <div v-for="product in products" :key="product.id">
+      <div class="productBlock">
+        <h1>
+          {{ product.name }}
+        </h1>
+        <br />
+        <img v-bind:src="product.image_url" v-bind:alt="product.name" class="productImage" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.productBlock {
+  border-style: solid;
+  border-color: black;
+  display: inline-block;
+  width: 500px;
+  padding: 10px;
+  margin: 10px;
+}
+
+.productImage {
+  width: 300px;
+}
+</style>
